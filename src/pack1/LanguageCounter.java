ff24ece0ea8f51c7b908f96c5fda69debd7ef2f3
pucha <pucha@Anusha>
2024-05-18 10:09:29 +0530

    Languages

---
+++ src/pack1/LanguageCounter.java
@@ -0,0 +1,30 @@
+package pack1;
+
+import java.util.HashMap;
+
+public class LanguageCounter {
+
+	public static void main(String[] args) {
+		String languages="eng chinese eng telugu chinese hindi chinese";
+		String[] languageArray = languages.split(" ");
+		
+		HashMap<String,Integer> obj=new HashMap<String,Integer>();
+		for(String str : languageArray)
+		{
+			if(obj.containsKey(str))
+			{
+				obj.put(str,obj.get(str)+1);
+			}
+			else 
+			{
+				obj.put(str,1);
+			}
+		
+	
+		System.out.println(obj);
+
+	}
+
+
+	}
+}
